cc0b44d8368d97acf02e747e3cf92e82ecf0ac4a
Juan Andrade <juan.andrade817@gmail.com>
2023-02-07 14:59:44 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/juan/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.juan.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juan.cursomc.domain.Pedido;
+import com.juan.cursomc.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! id: " + ", Tipo: " + Pedido.class.getName(), id));
+	}
+}
